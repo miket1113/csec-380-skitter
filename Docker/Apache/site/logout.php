92ae6b2a9e9bb36a3a5d0a2906f003b03a6acfb0
Michael Rhodes <mjr2563@rit.edu>
2018-12-01 17:44:16 -0800

    follow,unfollow,userSearch

--- Docker/Apache/site/logout.php
+++ Docker/Apache/site/logout.php
@@ -3,6 +3,13 @@ session_start();
 header('Cache-Control: no-cache, no-store, must-revalidate');
 header('Pragma: no-cache');
 header('Expires: 0');
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
 session_start();
 session_destroy();
 
